0a7e5ea3e2404d4a0deb6aa197df1e114f136d6b
agrzelak26 <aleksandragrzelak26@gmail.com>
2023-03-17 10:14:33 +0100

    Initial commit

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,42 +1,18 @@
 import java.util.Locale;
 
-public class Polygon extends Shape {
-    private Point[] arr;
-    private Style style;
+public class Polygon implements Shape {
 
-    public Polygon(int count, Style style){
-        this.style = style;
-        arr = new Point[count];
-    }
-    public Polygon(int count){
-        arr = new Point[count];
-    }
+    private Vec2[] points;
 
-    public void setPoint(int index, Point point){
-        arr[index] = point;
-    }
-    public static Polygon square(Segment segment, Style style){
-        Point point = new Point((segment.getA().x + segment.getB().y)/2, (segment.getB().x + segment.getB().y)/2);
-        Segment segmentprostopadly = Segment.perpendicular(segment, point)[0];
-        Point[] points = {segment.getA(), segmentprostopadly.getA(), segment.getB(), segmentprostopadly.getB()};
-        Polygon kwadrat = new Polygon(4);
-        kwadrat.setPoints(points);
-        return kwadrat;
-    }
-
-    public void setPoints(Point[] points){
-        arr = points;
+    public Polygon(Vec2[] points) {
+        this.points = points;
     }
 
     public String toSvg() {
         String pointsString = "";
-        for(Point point : arr) {
-            pointsString += point.x + "," + point.y + " ";
-        }
-        String styleString = "";
-        if (this.style != null) {
-            styleString = this.style.toSvg();
-        }
-        return String.format(Locale.ENGLISH, "<polygon points= \"%s\"%s />", pointsString, styleString);
+        for(Vec2 vec2 : points)
+            pointsString += vec2.x + "," + vec2.y + " ";
+        return String.format(Locale.ENGLISH,"<polygon points=\"%s\" />",
+                pointsString);
     }
 }
--- src/Shape.java
+++ src/Shape.java
@@ -1,12 +1,3 @@
-public abstract class Shape {
-    protected Style style;
-
-    public Shape(Style s) {
-        style = s;
-    }
-    public Shape(){
-        style = new Style("none", "black", 1);
-    }
-
-    public abstract String toSvg();
+public interface Shape {
+    String toSvg();
 }
